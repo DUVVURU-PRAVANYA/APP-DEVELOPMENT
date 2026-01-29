45c89f5c010d07e893db61b494b3dcf44ec0e00a
DUVVURU PRAVANYA SREE <150267834+DUVVURU-PRAVANYA@users.noreply.github.com>
2026-01-29 23:26:48 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,21 @@
+const CACHE_NAME = 'botanica-v1';
+
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/flora.png'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('botanica-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/shan.html'
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
